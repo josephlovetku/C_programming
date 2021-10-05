bb0effb06fab32b51c56d625acd058a55cfa3d3c
josephlovetku <91767797+josephlovetku@users.noreply.github.com>
2021-10-05 10:55:56 +0800

    Update political_game.h

--- political_game.h
+++ political_game.h
@@ -20,21 +20,18 @@ friend void SetExpUti(Candidate &, Candidate &);
 friend void SetExpUti(Candidate &, Candidate &, std::string type);
 public:
 	Candidate () { init_uti(); }
-	//Candidate (int ut1, int ut2): utSup(ut1), utOpp(ut2) {}
-	Candidate (int ut1, int ut2, int ut3): utSup(ut1), utOpp(ut2), utSwi(ut3) {}
+	Candidate (int ut1, int ut2): utSup(ut1), utOpp(ut2) {}
 	void init_uti();
 	void set_uti(int, int, int);
 	int get_sup();
 	int get_opp();
-    int get_swi();
-	int uti_sum();
+    	int uti_sum();
 	double get_expUti();
 	double get_expSW();
 	static constexpr int utUB = 1000;
 private:
 	int utSup;
 	int utOpp;
-	int utSwi;
 	double expUti;  // expected payoff
     double expSW; // expected social welfare
 }; 
@@ -47,7 +44,6 @@ struct Party {
 struct Game { 
 	Party A, B;
 	bool egoismCheck(Party &, Party &);
-	bool betterThanSwing(Party &, Party &);
 };
 
 bool checkNE(vector<vector<double>> & SocialWelfare); 
