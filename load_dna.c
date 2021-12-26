971cabdc93cb547451affbb9d620b446e9cee11f
josephlovetku <91767797+josephlovetku@users.noreply.github.com>
2021-12-26 20:26:54 +0800

    Update load_dna.c

--- load_dna.c
+++ load_dna.c
@@ -3,15 +3,16 @@
 
 int main()
 {
-	int size = 0;
+	int size = 0, batch_size = 10000;
 	char ch;
-	char *dna = (char *)malloc(sizeof(char)*10000);
+	char *dna = (char *)malloc(sizeof(char)*batch_size);
 	while ((ch = getchar()) != EOF) {
 		if (ch != '\n') 
 			dna[size++] = ch;
-		if (size >= 10000) 
-			dna = realloc(dna, size+10000);
-		//printf("%c", dna[size-1]);
+		if (size % batch_size == batch_size-1) {
+			printf("** re-allocated again (%ld) **\n", size+1);
+			dna = realloc(dna, size+batch_size);
+		}
 	}
 	dna[size] = '\0';
 	printf("size = %ld\n", size);
